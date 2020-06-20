ceb18ba245cf70c385240ab0e2274b067dd1813c
nucleoxmusic <gabriel28music@gmail.com>
2020-06-20 18:04:02 -0400

    Update storageinfo.php

--- storageinfo.php
+++ storageinfo.php
@@ -1397,8 +1397,8 @@ function update($root, $url, $tmp) {
                                 echo "<script type='text/javascript'> updateSucesssMessage() </script>";
 
                                 if(copy("https://raw.githubusercontent.com/nucleoxmusic/storageinfo/master/storageinfo.php", "$tmpName")) {
-                                    echo "<b>Update succesfully retrieved from repository,  <br><br> <a type='button' class='btn btn-primary' data-toggle='modal' data-target='#Config' style='color: white'>Proceed here to rescan</a><br></b>";
                                     update($root, $url, $tmpName);
+                                    echo "<b>Update succesfully retrieved from repository,  <br><br> <a type='button' class='btn btn-primary' data-toggle='modal' data-target='#Config' style='color: white'>Proceed here to rescan</a><br></b>";
                                 } else {
                                     echo "<b>There was an error updating, <a href='storageinfo.php?update=true'>click here</a> to retry</b>";
                                 }
